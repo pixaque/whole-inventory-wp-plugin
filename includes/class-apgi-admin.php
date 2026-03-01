b7c18e0d0213591e55b008a76140498a2e2524d1
Asad Ullah <pixaque@gmail.com>
2026-03-02 03:04:08 +0500

    fix settings page access for apgi_options URL

--- includes/class-apgi-admin.php
+++ includes/class-apgi-admin.php
@@ -20,8 +20,49 @@ class Admin {
 	 * Constructor.
 	 */
 	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'register_settings_page' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
 		add_filter( 'plugin_action_links_' . plugin_basename( APGI_PLUGIN_FILE ), array( $this, 'plugin_action_links' ) );
+	}
+
+	/**
+	 * Register plugin settings page endpoint.
+	 *
+	 * @return void
+	 */
+	public function register_settings_page(): void {
+		add_menu_page(
+			esc_html__( 'Inventory Settings', 'wer_pk' ),
+			esc_html__( 'Inventory Settings', 'wer_pk' ),
+			'manage_options',
+			Settings::OPTION_NAME,
+			array( $this, 'render_settings_page' ),
+			'dashicons-archive',
+			58
+		);
+	}
+
+	/**
+	 * Render settings page content.
+	 *
+	 * @return void
+	 */
+	public function render_settings_page(): void {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( esc_html__( 'Sorry, you are not allowed to access this page.', 'wer_pk' ) );
+		}
+
+		if ( class_exists( '\\Redux' ) ) {
+			if ( method_exists( '\\Redux', 'render_panel' ) ) {
+				\Redux::render_panel( Settings::OPTION_NAME );
+				return;
+			}
+
+			do_action( 'redux/page/' . Settings::OPTION_NAME . '/render' );
+			return;
+		}
+
+		echo '<div class="wrap"><h1>' . esc_html__( 'Inventory Settings', 'wer_pk' ) . '</h1><p>' . esc_html__( 'Redux Framework is required to render the settings panel.', 'wer_pk' ) . '</p></div>';
 	}
 
 	/**
