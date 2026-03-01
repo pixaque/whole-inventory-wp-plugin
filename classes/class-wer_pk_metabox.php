9a61df1783600db2721090665b231a93fdf8912b
Asad Ullah <pixaque@gmail.com>
2026-03-02 01:34:27 +0500

    Extend Meta Box settings to registration and form labels

--- classes/class-wer_pk_metabox.php
+++ classes/class-wer_pk_metabox.php
@@ -24,7 +24,8 @@ if ( ! class_exists( 'WER_PK_MetaBox', false ) ) :
 		 * @return array
 		 */
 		public static function register_meta_boxes( $meta_boxes ) {
-			$login_page_id = self::get_page_id_by_path( 'login' );
+			$login_page_id        = self::get_page_id_by_path( 'login' );
+			$registration_page_id = self::get_page_id_by_path( 'registration' );
 
 			if ( $login_page_id ) {
 				$meta_boxes[] = array(
@@ -36,28 +37,113 @@ if ( ! class_exists( 'WER_PK_MetaBox', false ) ) :
 					),
 					'fields'     => array(
 						array(
-							'id'      => 'wer_pk_login_heading',
-							'name'    => __( 'Form Heading', 'wer_pk' ),
-							'type'    => 'text',
-							'std'     => __( 'Login', 'wer_pk' ),
+							'id'   => 'wer_pk_login_heading',
+							'name' => __( 'Form Heading', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Login', 'wer_pk' ),
 						),
 						array(
-							'id'      => 'wer_pk_login_button_text',
-							'name'    => __( 'Button Label', 'wer_pk' ),
-							'type'    => 'text',
-							'std'     => __( 'Log In', 'wer_pk' ),
+							'id'   => 'wer_pk_login_username_label',
+							'name' => __( 'Username Label', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Username or Email Address', 'wer_pk' ),
 						),
 						array(
-							'id'      => 'wer_pk_login_show_register_link',
-							'name'    => __( 'Show registration link', 'wer_pk' ),
-							'type'    => 'checkbox',
-							'std'     => 1,
+							'id'   => 'wer_pk_login_password_label',
+							'name' => __( 'Password Label', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Password', 'wer_pk' ),
 						),
 						array(
-							'id'      => 'wer_pk_login_register_url',
-							'name'    => __( 'Registration URL', 'wer_pk' ),
-							'type'    => 'url',
-							'std'     => home_url( '/registration/' ),
+							'id'   => 'wer_pk_login_button_text',
+							'name' => __( 'Button Label', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Log In', 'wer_pk' ),
+						),
+						array(
+							'id'   => 'wer_pk_login_forgot_password_label',
+							'name' => __( 'Forgot Password Label', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Forgot Password?', 'wer_pk' ),
+						),
+						array(
+							'id'   => 'wer_pk_login_register_label',
+							'name' => __( 'Register Label', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Register', 'wer_pk' ),
+						),
+						array(
+							'id'   => 'wer_pk_login_show_register_link',
+							'name' => __( 'Show registration link', 'wer_pk' ),
+							'type' => 'checkbox',
+							'std'  => 1,
+						),
+						array(
+							'id'   => 'wer_pk_login_register_url',
+							'name' => __( 'Registration URL', 'wer_pk' ),
+							'type' => 'url',
+							'std'  => home_url( '/registration/' ),
+						),
+					),
+				);
+			}
+
+			if ( $registration_page_id ) {
+				$meta_boxes[] = array(
+					'id'         => 'wer_pk_registration_page_settings',
+					'title'      => __( 'Inventory Registration Page Settings', 'wer_pk' ),
+					'post_types' => array( 'page' ),
+					'include'    => array(
+						'ID' => array( $registration_page_id ),
+					),
+					'fields'     => array(
+						array(
+							'id'   => 'wer_pk_registration_heading',
+							'name' => __( 'Form Heading', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Register', 'wer_pk' ),
+						),
+						array(
+							'id'   => 'wer_pk_registration_username_label',
+							'name' => __( 'Username Label', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Username', 'wer_pk' ),
+						),
+						array(
+							'id'   => 'wer_pk_registration_email_label',
+							'name' => __( 'Email Label', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Email', 'wer_pk' ),
+						),
+						array(
+							'id'   => 'wer_pk_registration_password_label',
+							'name' => __( 'Password Label', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Password', 'wer_pk' ),
+						),
+						array(
+							'id'   => 'wer_pk_registration_button_text',
+							'name' => __( 'Button Label', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Register', 'wer_pk' ),
+						),
+						array(
+							'id'   => 'wer_pk_registration_show_login_link',
+							'name' => __( 'Show login link', 'wer_pk' ),
+							'type' => 'checkbox',
+							'std'  => 1,
+						),
+						array(
+							'id'   => 'wer_pk_registration_login_label',
+							'name' => __( 'Login Link Label', 'wer_pk' ),
+							'type' => 'text',
+							'std'  => __( 'Already have an account? Login', 'wer_pk' ),
+						),
+						array(
+							'id'   => 'wer_pk_registration_login_url',
+							'name' => __( 'Login URL', 'wer_pk' ),
+							'type' => 'url',
+							'std'  => home_url( '/login/' ),
 						),
 					),
 				);
--- templates/registration.php
+++ templates/registration.php
@@ -1,13 +1,81 @@
-<form method="post" action="">
-    <?php wp_nonce_field('custom_registration_nonce_action', 'custom_registration_nonce'); ?>
-    <label for="username"><?php echo __("Username", "wer_pk"); ?></label>
-    <input type="text" name="username" required>
+<?php
+$current_page_id = get_queried_object_id();
 
-    <label for="email"><?php echo __("Email", "wer_pk"); ?></label>
-    <input type="email" name="email" required>
+$registration_heading = __( 'Register', 'wer_pk' );
+$username_label = __( 'Username', 'wer_pk' );
+$email_label = __( 'Email', 'wer_pk' );
+$password_label = __( 'Password', 'wer_pk' );
+$register_button_text = __( 'Register', 'wer_pk' );
+$show_login_link = true;
+$login_link_label = __( 'Already have an account? Login', 'wer_pk' );
+$login_url = home_url( '/login/' );
 
-    <label for="password"><?php echo __("Password", "wer_pk"); ?></label>
-    <input type="password" name="password" required>
+if ( function_exists( 'rwmb_get_value' ) && $current_page_id ) {
+	$registration_heading_meta = rwmb_get_value( 'wer_pk_registration_heading', array(), $current_page_id );
+	$username_label_meta = rwmb_get_value( 'wer_pk_registration_username_label', array(), $current_page_id );
+	$email_label_meta = rwmb_get_value( 'wer_pk_registration_email_label', array(), $current_page_id );
+	$password_label_meta = rwmb_get_value( 'wer_pk_registration_password_label', array(), $current_page_id );
+	$register_button_text_meta = rwmb_get_value( 'wer_pk_registration_button_text', array(), $current_page_id );
+	$show_login_link_meta = rwmb_get_value( 'wer_pk_registration_show_login_link', array(), $current_page_id );
+	$login_link_label_meta = rwmb_get_value( 'wer_pk_registration_login_label', array(), $current_page_id );
+	$login_url_meta = rwmb_get_value( 'wer_pk_registration_login_url', array(), $current_page_id );
 
-    <input type="submit" name="wer_pk_registration_submit" value="<?php echo __("Register", "wer_pk"); ?>">
-</form>
+	if ( ! empty( $registration_heading_meta ) ) {
+		$registration_heading = $registration_heading_meta;
+	}
+
+	if ( ! empty( $username_label_meta ) ) {
+		$username_label = $username_label_meta;
+	}
+
+	if ( ! empty( $email_label_meta ) ) {
+		$email_label = $email_label_meta;
+	}
+
+	if ( ! empty( $password_label_meta ) ) {
+		$password_label = $password_label_meta;
+	}
+
+	if ( ! empty( $register_button_text_meta ) ) {
+		$register_button_text = $register_button_text_meta;
+	}
+
+	if ( '' !== $show_login_link_meta && null !== $show_login_link_meta ) {
+		$show_login_link = (bool) $show_login_link_meta;
+	}
+
+	if ( ! empty( $login_link_label_meta ) ) {
+		$login_link_label = $login_link_label_meta;
+	}
+
+	if ( ! empty( $login_url_meta ) ) {
+		$login_url = $login_url_meta;
+	}
+}
+?>
+<div class="frontend-registration-form">
+	<h2><?php echo esc_html( $registration_heading ); ?></h2>
+	<form method="post" action="">
+		<?php wp_nonce_field( 'custom_registration_nonce_action', 'custom_registration_nonce' ); ?>
+		<p>
+			<label for="username"><?php echo esc_html( $username_label ); ?></label>
+			<input type="text" id="username" name="username" required>
+		</p>
+		<p>
+			<label for="email"><?php echo esc_html( $email_label ); ?></label>
+			<input type="email" id="email" name="email" required>
+		</p>
+		<p>
+			<label for="password"><?php echo esc_html( $password_label ); ?></label>
+			<input type="password" id="password" name="password" required>
+		</p>
+		<p>
+			<input type="submit" name="wer_pk_registration_submit" value="<?php echo esc_attr( $register_button_text ); ?>">
+		</p>
+	</form>
+	<?php if ( $show_login_link ) : ?>
+		<p>
+			<a href="<?php echo esc_url( $login_url ); ?>"><?php echo esc_html( $login_link_label ); ?></a>
+		</p>
+	<?php endif; ?>
+</div>
